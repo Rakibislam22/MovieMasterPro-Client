041d2b7cdaec1db99ad1193d2660297cab3652c1
Rakibislam22 <rakibislam13122018@gmail.com>
2025-11-10 21:39:29 +0600

    Add Genre section

---
+++ src/components/homeComponenets/GenreSection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Film, Heart, Laugh, Zap, Sword, Ghost, Atom, Music } from "lucide-react";
+
+const genres = [
+  { name: "Action", icon: <Zap className="w-6 h-6 text-red-500" /> },
+  { name: "Drama", icon: <Heart className="w-6 h-6 text-pink-500" /> },
+  { name: "Comedy", icon: <Laugh className="w-6 h-6 text-yellow-400" /> },
+  { name: "Sci-Fi", icon: <Atom className="w-6 h-6 text-blue-400" /> },
+  { name: "Thriller", icon: <Sword className="w-6 h-6 text-purple-500" /> },
+  { name: "Horror", icon: <Ghost className="w-6 h-6 text-gray-400" /> },
+  { name: "Romance", icon: <Heart className="w-6 h-6 text-rose-400" /> },
+  { name: "Music", icon: <Music className="w-6 h-6 text-green-400" /> },
+  { name: "Animation", icon: <Film className="w-6 h-6 text-orange-400" /> },
+];
+
+const GenreSection = () => {
+  return (
+    <section className="py-16 px-6 md:px-12 ">
+      <div className="mb-12">
+        <h2 className="border-l-6 border-[#f97316] pl-3 text-4xl md:text-5xl font-bold mb-2">
+         Browse by Genre
+        </h2>
+        <p className="text-gray-400">Find movies from your favorite categories</p>
+      </div>
+
+      <div className="text-white mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 text-center">
+        {genres.map((genre, index) => (
+          <div
+            key={index}
+            className="bg-gray-900 rounded-xl p-6 flex flex-col items-center justify-center shadow-md hover:scale-[1.05] hover:bg-gray-800 transition-transform duration-300"
+          >
+            <div className="mb-3">{genre.icon}</div>
+            <h3 className="text-lg font-semibold">{genre.name}</h3>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default GenreSection;
